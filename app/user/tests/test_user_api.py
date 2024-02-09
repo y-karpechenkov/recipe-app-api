5b3897268e37d30ef75cd184638cfdbf4d5ad4e9
iegor.karpechenkov@fountainhead-it.com <iegor.karpechenkov@fountainhead-it.com>
2024-02-09 23:17:05 +0100

    Fixing /me urls setup

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -91,17 +91,25 @@ class PublicUserAPITest(TestCase):
         )
 
         payload = {
-            'email': 'test@example.com', 'password': 'badpass'
+            "email": "test@example.com", "password": "badpass"
         }
         res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
+
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
         """Test posting a blank password retirns an error."""
         payload = {
-            'email': 'test@example.com',
-            'password': ''
+            "email": "test@example.com",
+            "password": ""
         }
 
         res = self.client.post(TOKEN_URL, payload)
