f404e97b08d85044c8640b050c305cc30745e18a
iegor.karpechenkov@fountainhead-it.com <iegor.karpechenkov@fountainhead-it.com>
2024-02-19 22:45:25 +0100

    Adding Patch and Delete tags functionality

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -16,6 +16,11 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail URL."""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='user@example.com', password='testPass1234'):
     """Create and return user."""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -66,3 +71,28 @@ class PrivateTagAPITests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating the tag."""
+        tag = Tag.objects.create(user=self.user, name="Tag1")
+
+        payload = {
+            'name': 'TagUpdated'
+        }
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting the tag."""
+        tag = Tag.objects.create(user=self.user, name='Tag1')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -41,7 +41,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
     """Mange tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
